d14fb222254593f8d13e3dcec9a679e99293d33f
Kordian555 <kordian.borowy2@gmail.com>
2026-08-13 19:39:05 +0200

    add error controller

---
+++ src/Controller/ErrorController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\ErrorHandler\Exception\FlattenException;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Psr\Log\LoggerInterface;
+
+class ErrorController extends AbstractController
+{
+    public function __construct(
+        private LoggerInterface $logger
+    )
+    {
+    }
+
+    public function show(FlattenException $exception): Response
+    {
+        if (null !== $this->logger) {
+            $this->logger->error($exception->getMessage());
+        }
+
+        return new JsonResponse(['error' => $exception->getMessage()]);
+    }
+
+    public function error404(): Response
+    {
+        return new JsonResponse(['code' => 404, 'message' => 'Not found']);
+    }
+
+    public function error500(): Response
+    {
+        return new JsonResponse(['code' => 500, 'message' => 'Internal server error']);
+    }
+
+    public function error403(): Response
+    {
+        return new JsonResponse(['code' => 403, 'message' => 'Forbidden']);
+    }
+
+    public function error401(): Response
+    {
+        return new JsonResponse(['code' => 401, 'message' => 'Unauthorized']);
+    }
+
+    public function error400(): Response
+    {
+        return new JsonResponse(['code' => 400, 'message' => 'Bad request']);
+    }
+}
